98fff53828dc54cd898694a36f10757cf3720df5
Gabriel Lazarine <gabriellazarinesilva@gmail.com>
2026-04-14 12:33:48 -0300

    alteração de import no teste

--- ATIVIDADE3/calculadora_flutter/test/widget_test.dart
+++ ATIVIDADE3/calculadora_flutter/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calculadora_flutter/controllers/calculator_logic.dart';
+import 'package:calculadora_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
